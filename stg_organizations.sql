caf6cd132f623d5389b82ca95e85abe4dc3608d9
rajeshkashyap19 <52137080+rajeshkashyap19@users.noreply.github.com>
2025-06-01 07:29:58 +0100

    Update stg_organizations.sql

--- stg_organizations.sql
+++ stg_organizations.sql
@@ -1,1 +1,17 @@
+with orgs as (
+    select *
+    from {{ source('raw', 'organizations') }}
+),
 
+cleaned as (
+SELECT 
+/*convert the column formats to appropriate ones as there are inconsistencies noted in values*/
+    CAST(organization_id AS BIGINT) AS organization_id,
+    CAST(first_payment_date as DATE) AS first_payment_date,
+    CAST(last_payment_date as DATE) AS last_payment_date,
+    CAST(legal_entity_country_code AS STRING) AS legal_entity_country_code,
+    CAST(created_date AS DATETIME) AS created_date
+FROM {{ source('your_source', 'organizations') }}
+)
+
+SELECT * FROM cleaned

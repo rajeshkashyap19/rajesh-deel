9841934d5576399575be9b5cd6c19c2a6bf4c8a5
rajeshkashyap19 <52137080+rajeshkashyap19@users.noreply.github.com>
2025-06-01 07:40:49 +0100

    Update stg_invoices.sql
    clean version of the invoices file

--- stg_invoices.sql
+++ stg_invoices.sql
@@ -1,1 +1,25 @@
+with invoices as (
+    select *
+    from {{ source('raw', 'invoices') }}
+),
 
+cleaned as (
+SELECT 
+/*convert the column formats to appropriate ones as there are inconsistencies noted in values*/
+    CAST(invoice_id AS BIGINT) AS invoice_id,
+    CAST(parent_invoice_id AS BIGINT) AS parent_invoice_id,
+    CAST(transaction_id AS BIGINT) AS transaction_id,
+    CAST(organization_id AS BIGINT) AS organization_id,
+    type,
+    status,
+    currency,
+    payment_currency,
+    payment_method,
+    amount,
+    payment_amount,
+    fx_rate,
+    fx_rate_payment
+FROM invoices
+)
+
+SELECT * FROM cleaned
